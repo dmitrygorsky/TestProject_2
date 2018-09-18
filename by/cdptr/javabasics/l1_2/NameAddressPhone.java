96c09e9921fc896d89b769c5443670042d8892be
dmitrygorsky <43175416+dmitrygorsky@users.noreply.github.com>
2018-09-18 13:06:35 +0300

    Initial commit

---
+++ by/cdptr/javabasics/l1_2/NameAddressPhone.java
@@ -0,0 +1,12 @@
+package by.cdptr.javabasics.l1_2;
+
+public class NameAddressPhone {
+
+    public static void main(String[] args) {
+
+        System.out.println("Меня зовут Дмитрий Горский");
+        System.out.println("Мой адрес не дом и не улица, мой адрес - Советский Союз!");
+        System.out.println("Мой телефон - +1234567890");
+
+    }
+}
